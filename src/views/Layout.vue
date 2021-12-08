bf5b80aea2a1336d163414a1806cf29c0f379191
SheepNDW <hank3681@gmail.com>
2021-12-08 20:32:25 +0800

    完成吸頂頭部元件開發

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
   <AppNavBar />
   <!-- 頭部元件 -->
   <AppHeader />
+  <!-- 吸頂頭部元件 -->
+  <AppHeaderSticky />
   <!-- 內容容器 -->
   <div class="app-body">
     <!-- 二級路由 -->
@@ -16,10 +18,11 @@
 import AppNavBar from '@/components/app-navbar.vue';
 import AppHeader from '@/components/app-header.vue';
 import AppFooter from '@/components/app-footer.vue';
+import AppHeaderSticky from '@/components/app-header-sticky.vue';
 import { useStore } from 'vuex';
 export default {
   name: 'Layout',
-  components: { AppNavBar, AppHeader, AppFooter },
+  components: { AppNavBar, AppHeader, AppFooter, AppHeaderSticky },
   // 獲取分類資料
   setup() {
     const store = useStore();
