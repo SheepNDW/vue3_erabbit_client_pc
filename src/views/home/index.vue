eb129f12b651e6dcad2adcfb3a9d4214445c4f63
SheepNDW <hank3681@gmail.com>
2021-12-08 21:09:55 +0800

    左側分類-結構渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="container">HomePage</div>
+  <div class="container">
+    <!-- 首頁入口 -->
+    <div class="home-entry">
+      <!-- 左側導覽 -->
+      <HomeCategory />
+      <!-- 輪播圖 -->
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCategory from './components/home-category.vue';
 export default {
   name: 'HomePage',
+  components: { HomeCategory },
 };
 </script>
 
-<style lang="scss" scoped>
-.container {
-  @include hoverShadow();
-  color: $xtxColor;
-}
-</style>
+<style lang="scss" scoped></style>
