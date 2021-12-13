6aad137cad435848479a4a966bcd34199b86479a
SheepNDW <hank3681@gmail.com>
2021-12-13 14:30:21 +0800

    批量註冊元件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,21 +1,36 @@
 // 擴充vue原有的功能: 全局元件, 自定義指令, 掛載原型方法
 
 import defaultImg from '@/assets/images/200.png'
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
+
+
+// 導入library資料夾下的所有元件
+// 批量導入需要使用一个函式 require.context(dir,deep,matching)
+// 參數：1. 目錄  2. 是否加載子目錄  3. 加載檔案的正則匹配
+const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install(app) {
     // 在app上進行擴展, app提供 component directive 函式
     // 如果要掛載原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
+
+    // 根據keys 批量註冊
+    importFn.keys().forEach(path => {
+      // 導入元件
+      const component = importFn(path).default
+      // 進行註冊
+      app.component(component.name, component)
+    })
+
     // 定義指令
     defineDirective(app)
   }
