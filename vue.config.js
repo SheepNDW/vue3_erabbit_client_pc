10bc756bf836ae1303598064e5ccb81d0a2de5b5
SheepNDW <hank3681@gmail.com>
2021-12-28 10:55:29 +0800

    將圖片轉換成base64

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,12 @@ module.exports = {
         path.join(__dirname, './src/assets/styles/mixins.scss')
       ]
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10000 }))
   }
 }
