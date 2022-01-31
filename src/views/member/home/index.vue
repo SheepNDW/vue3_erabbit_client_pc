e697f9dd2bf464d04ad0fee1bb86beea2d85b274
SheepNDW <hank3681@gmail.com>
2022-01-31 14:48:34 +0800

    渲染個人中心頁面

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="member-home">MemberHome</div>
+  <div class="member-home">
+    <!-- 概覽 -->
+    <HomeOverview />
+    <!-- 收藏 -->
+    <HomePanel title="我的收藏">
+      <GoodsItem v-for="i in 4" :key="i" :goods="goods" />
+    </HomePanel>
+    <!-- 足跡 -->
+    <HomePanel title="我的足跡">
+      <GoodsItem v-for="i in 4" :key="i" :goods="goods" />
+    </HomePanel>
+    <!-- 猜你 -->
+    <GoodsRelevant />
+  </div>
 </template>
 
 <script>
+import HomeOverview from './components/home-overview.vue';
+import HomePanel from './components/home-panel.vue';
+import GoodsRelevant from '@/views/goods/components/goods-relevant.vue';
+import GoodsItem from '@/views/category/components/goods-item.vue';
 export default {
   name: 'MemberHome',
+  components: { HomeOverview, HomePanel, GoodsRelevant, GoodsItem },
+  setup() {
+    const goods = {
+      id: '1',
+      name: '自煮火锅不排队 麦饭石不粘鸳鸯火锅',
+      picture: 'https://yanxuan-item.nosdn.127.net/fcdcb840a0f5dd754bb8fd2157579012.jpg',
+      desc: '清汤鲜香 红汤劲爽',
+      price: '159.00',
+    };
+    return { goods };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep(.xtx-carousel) .carousel-btn.prev {
+  left: 5px;
+}
+:deep(.xtx-carousel) .carousel-btn.next {
+  right: 5px;
+}
+</style>
