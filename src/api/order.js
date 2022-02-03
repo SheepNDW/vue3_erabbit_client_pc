96c2fbdddbf217eb28cad4941d1da59fc779f72e
SheepNDW <hank3681@gmail.com>
2022-02-03 15:29:38 +0800

    完成訂單管理-列表渲染

--- src/api/order.js
+++ src/api/order.js
@@ -43,3 +43,13 @@ export const findOrderDetail = (id) => {
   return request('/member/order/' + id, 'get')
 }
 
+/**
+ * 獲取訂單列表
+ * @param {Number} page - 頁碼
+ * @param {Number} pageSize - 每頁條數
+ * @param {Number} orderState - 訂單狀態，1為待付款、2為待發貨、3為待收货、4為待評價、5為已完成、6為已取消，未傳該參數或0為全部
+ * @returns
+ */
+export const findOrderList = ({ page = 1, pageSize = 10, orderState = 0 }) => {
+  return request('/member/order', 'get', { page, pageSize, orderState })
+}
---
+++ src/views/member/order/components/order-item.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="order-item">
+    <div class="head">
+      <span>下單時間：{{ order.createTime }}</span>
+      <span>訂單編號：{{ order.id }}</span>
+      <span class="down-time" v-if="order.orderState === 1">
+        <i class="iconfont icon-down-time"></i>
+        <b>付款截止：{{ timeText }}</b>
+      </span>
+      <a href="javascript:;" class="del" v-if="[5, 6].includes(order.orderState)">刪除</a>
+    </div>
+    <div class="body">
+      <div class="column goods">
+        <ul>
+          <li v-for="goods in order.skus" :key="goods.id">
+            <RouterLink class="image" :to="`/product/${goods.id}`">
+              <img :src="goods.image" alt="" />
+            </RouterLink>
+            <div class="info">
+              <p class="name ellipsis-2">{{ goods.name }}</p>
+              <p class="attr ellipsis">{{ goods.attrsText }}</p>
+            </div>
+            <div class="price">¥{{ goods.realPay }}</div>
+            <div class="count">x{{ goods.quantity }}</div>
+          </li>
+        </ul>
+      </div>
+      <div class="column state">
+        <p>{{ orderStatus[order.orderState].label }}</p>
+        <!-- 待收貨：查看物流 -->
+        <!-- 待評價：評價商品 -->
+        <!-- 已完成：查看評價 -->
+        <p v-if="order.orderState === 3">
+          <a class="green" href="javascript:;">查看物流</a>
+        </p>
+        <p v-if="order.orderState === 4">
+          <a class="green" href="javascript:;">評價商品</a>
+        </p>
+        <p v-if="order.orderState === 5">
+          <a class="green" href="javascript:;">查看評價</a>
+        </p>
+      </div>
+      <div class="column amount">
+        <p class="red">¥{{ order.payMoney }}</p>
+        <p>（含運費：¥{{ order.postFee }}）</p>
+        <p>線上支付</p>
+      </div>
+      <div class="column action">
+        <!-- 待支付：立即付款，查看詳情，取消訂單 -->
+        <!-- 待發貨：查看詳情，再次購買 -->
+        <!-- 待收貨：確認收貨，查看詳情，再次購買 -->
+        <!-- 待評價：查看詳情，再次購買，申請售後 -->
+        <!-- 已完成：查看詳情，再次購買，申請售後 -->
+        <!-- 已取消：查看詳情 -->
+        <XtxButton
+          @click="$router.push(`/member/pay?orderId=${order.id}`)"
+          v-if="order.orderState === 1"
+          type="primary"
+          size="small"
+          >立即付款</XtxButton
+        >
+        <XtxButton v-if="order.orderState === 3" type="primary" size="small"
+          >確認收貨</XtxButton
+        >
+        <p><a href="javascript:;">查看詳情</a></p>
+        <p v-if="order.orderState === 1"><a href="javascript:;">取消訂單</a></p>
+        <p v-if="[2, 3, 4, 5].includes(order.orderState)">
+          <a href="javascript:;">再次購買</a>
+        </p>
+        <p v-if="[4, 5].includes(order.orderState)">
+          <a href="javascript:;">申請售後</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { orderStatus } from '@/api/constants';
+import { usePayTime } from '@/hooks';
+export default {
+  name: 'OrderItem',
+  props: {
+    order: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const { start, timeText } = usePayTime();
+    start(props.order.countdown);
+
+    return { orderStatus, timeText };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.order-item {
+  margin-bottom: 20px;
+  border: 1px solid #f5f5f5;
+  .head {
+    height: 50px;
+    line-height: 50px;
+    background: #f5f5f5;
+    padding: 0 20px;
+    overflow: hidden;
+    span {
+      margin-right: 20px;
+      &.down-time {
+        margin-right: 0;
+        float: right;
+        i {
+          vertical-align: middle;
+          margin-right: 3px;
+        }
+        b {
+          vertical-align: middle;
+          font-weight: normal;
+        }
+      }
+    }
+    .del {
+      margin-right: 0;
+      float: right;
+      color: #999;
+    }
+  }
+  .body {
+    display: flex;
+    align-items: stretch;
+    .column {
+      border-left: 1px solid #f5f5f5;
+      text-align: center;
+      padding: 20px;
+      > p {
+        padding-top: 10px;
+      }
+      &:first-child {
+        border-left: none;
+      }
+      &.goods {
+        flex: 1;
+        padding: 0;
+        align-self: center;
+        ul {
+          li {
+            border-bottom: 1px solid #f5f5f5;
+            padding: 10px;
+            display: flex;
+            &:last-child {
+              border-bottom: none;
+            }
+            .image {
+              width: 70px;
+              height: 70px;
+              border: 1px solid #f5f5f5;
+            }
+            .info {
+              width: 220px;
+              text-align: left;
+              padding: 0 10px;
+              p {
+                margin-bottom: 5px;
+                &.name {
+                  height: 38px;
+                }
+                &.attr {
+                  color: #999;
+                  font-size: 12px;
+                  span {
+                    margin-right: 5px;
+                  }
+                }
+              }
+            }
+            .price {
+              width: 100px;
+            }
+            .count {
+              width: 80px;
+            }
+          }
+        }
+      }
+      &.state {
+        width: 120px;
+        .green {
+          color: $xtxColor;
+        }
+      }
+      &.amount {
+        width: 200px;
+        .red {
+          color: $priceColor;
+        }
+      }
+      &.action {
+        width: 140px;
+        a {
+          display: block;
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
