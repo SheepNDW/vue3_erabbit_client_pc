2bd2d8bda2fb396e495be2ba7f7c8ea11f150170
SheepNDW <hank3681@gmail.com>
2022-02-06 16:07:55 +0800

    訂單詳情-頭部展示

---
+++ src/views/member/order/components/detail-action.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="detail-action">
+    <div class="state">
+      <span class="iconfont icon-order-unpay"></span>
+      <p>{{ orderStatus[order.orderState].label }}</p>
+    </div>
+    <div class="info">
+      <p>訂單編號：{{ order.id }}</p>
+      <p>下單時間：{{ order.createTime }}</p>
+    </div>
+    <div class="btn">
+      <!-- 待付款 -->
+      <template v-if="order.orderState === 1">
+        <XtxButton
+          @click="$router.push('/member/pay?id=' + order.id)"
+          type="primary"
+          size="small"
+          >立即付款</XtxButton
+        >
+        <XtxButton type="gray" size="small">取消訂單</XtxButton>
+      </template>
+      <!-- 待發貨 -->
+      <template v-if="order.orderState === 2">
+        <XtxButton type="primary" size="small">再次購買</XtxButton>
+      </template>
+      <!-- 待收貨 -->
+      <template v-if="order.orderState === 3">
+        <XtxButton type="primary" size="small">確認收貨</XtxButton>
+        <XtxButton type="plain" size="small">再次購買</XtxButton>
+      </template>
+      <!-- 待評價 -->
+      <template v-if="order.orderState === 4">
+        <XtxButton type="primary" size="small">再次購買</XtxButton>
+        <XtxButton type="plain" size="small">評價商品</XtxButton>
+        <XtxButton type="gray" size="small">申請售後</XtxButton>
+      </template>
+      <!-- 已完成 -->
+      <template v-if="order.orderState === 5">
+        <XtxButton type="primary" size="small">再次購買</XtxButton>
+        <XtxButton type="plain" size="small">查看評價</XtxButton>
+        <XtxButton type="gray" size="small">申請售後</XtxButton>
+      </template>
+      <!-- 已取消 -->
+    </div>
+  </div>
+</template>
+<script>
+import { orderStatus } from '@/api/constants';
+export default {
+  name: 'OrderDetailAction',
+  props: {
+    order: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup() {
+    return { orderStatus };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.detail-action {
+  height: 180px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  .state {
+    width: 220px;
+    text-align: center;
+    .iconfont {
+      font-size: 40px;
+      color: $xtxColor;
+    }
+    p {
+      font-size: 16px;
+      color: #666;
+      margin-bottom: 10px;
+    }
+  }
+  .info {
+    width: 240px;
+    line-height: 30px;
+    p {
+      color: #999;
+    }
+  }
+  .btn {
+    flex: 1;
+    text-align: right;
+    margin-right: 100px;
+    .xtx-button {
+      margin-left: 20px;
+    }
+  }
+}
+</style>
--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="MemberDetail">MemberDetail</div>
+  <div class="order-detail-page" v-if="order">
+    <!-- 頭部 -->
+    <DetailAction :order="order" />
+    <!-- 進度 -->
+    <!-- 物流 -->
+    <!-- 信息 -->
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import DetailAction from './components/detail-action.vue';
+import { findOrderDetail } from '@/api/order';
+import { useRoute } from 'vue-router';
 export default {
   name: 'MemberDetail',
+  components: { DetailAction },
+  setup() {
+    const route = useRoute();
+    const order = ref(null);
+    findOrderDetail(route.params.id).then(data => {
+      order.value = data.result;
+    });
+
+    return { order };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.order-detail-page {
+  background-color: #fff;
+  height: 100%;
+}
+</style>
